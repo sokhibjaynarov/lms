824c9f3875d1624faf1a90964e014e8afd16e2a5
SohibJaynarov <jaynarovsohib@gmail.com>
2021-09-23 15:24:54 +0500

    commited new files

--- school.py
+++ school.py
@@ -1,1 +1,24 @@
-print("hello world")
+#add studesnt and teacher
+#all student teacher return
+
+from Models.Teacher import Teacher
+from Models.Student import Student
+
+class School:
+    def __init__(self):
+        self.teachers = []
+        self.students = []
+
+    def addTeacher(self, teachars:Teacher):
+        self.teachers.append(teachars)
+
+    def addStudents(self, students:Student):
+        self.students.append(students)
+
+    def allTeachers(self):
+        return self.teachers
+
+    def allStudents(self):
+        return self.students
+
+
